6325170e150ffd83f2dbc4b9207f82e0062a3a1d
Anderson Moroni <andersonmoroni92@gmail.com>
2024-08-14 19:41:02 -0300

    feat: adicionado o component header e css

---
+++ src/extensions/components/Header.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+
+import './Masterpage.css'
+import './MasterpageMobile.css'
+const icon_card = require('../images/cad.svg')
+const logo = require('../images/logo.png')
+const icon_hamb = require('../images/menuhambuger.png')
+const icon_close = require('../images/close.svg')
+
+const Header = () => {
+    
+    return (
+        <div>
+            <div id="header">
+                <div className="container-fluid" id="link-rapido">
+                    <button className="btn-cadastrar">
+                        <img src={icon_card} alt="" />
+                        <span>Cadastrar</span>
+                    </button>
+                    <ul id="icons" className="nav justify-content-end icons">
+                    </ul>
+                    <ul id="links" className="nav justify-content-end links">
+                    </ul>
+                </div>
+                <div className="containet-fluid" id="header-body">
+                    <a href="/sites/Procel/">
+                        <img className="logo" src={logo} alt="Logo" />
+                    </a>
+                    <div className="right-side desktop">
+                        <input type="text" placeholder="O que você procura neste site?" />
+                        <button className="btn-openmenu navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <img className="" src={icon_hamb} alt="btn-menu" />
+                        </button>
+                    </div>
+                </div>
+                <div className="right-side search-mobile">
+                    <input type="text" placeholder="O que você procura?" />
+                    <button type="button" className="btn btn-clean-search">
+                        <img src={icon_close} alt="limpar" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/extensions/masterPageSolutionHeaderFooter/MasterPageSolutionHeaderFooterApplicationCustomizer.ts
+++ src/extensions/masterPageSolutionHeaderFooter/MasterPageSolutionHeaderFooterApplicationCustomizer.ts
@@ -1,38 +1,120 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
+import { escape } from '@microsoft/sp-lodash-subset';
 import {
-  BaseApplicationCustomizer
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName
 } from '@microsoft/sp-application-base';
 import { Dialog } from '@microsoft/sp-dialog';
+import Header from '../components/Header';
 
 import * as strings from 'MasterPageSolutionHeaderFooterApplicationCustomizerStrings';
 
 const LOG_SOURCE: string = 'MasterPageSolutionHeaderFooterApplicationCustomizer';
 
-/**
- * If your command set uses the ClientSideComponentProperties JSON input,
- * it will be deserialized into the BaseExtension.properties object.
- * You can define an interface to describe it.
- */
-export interface IMasterPageSolutionHeaderFooterApplicationCustomizerProperties {
-  // This is an example; replace with your own property
+export interface IMasterApplicationCustomizerProperties {
+  Top: string;
+  Bottom: string;
   testMessage: string;
 }
 
-/** A Custom Action which can be run during execution of a Client Side Application */
+export interface IMasterPageSolutionHeaderFooterApplicationCustomizerProperties {
+  Top: string;
+  Bottom: string;
+  testMessage: string;
+}
+
 export default class MasterPageSolutionHeaderFooterApplicationCustomizer
   extends BaseApplicationCustomizer<IMasterPageSolutionHeaderFooterApplicationCustomizerProperties> {
+
+    
+  // These have been added
+  private _topPlaceholder: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
+  private _onDispose(): void {
+    console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+  }
+
+  private _renderPlaceHolders(): void {
+    console.log("HelloWorldApplicationCustomizer._renderPlaceHolders()");
+    console.log(
+      "Available placeholders: ",
+      this.context.placeholderProvider.placeholderNames
+        .map(name => PlaceholderName[name])
+        .join(", ")
+    );
+  
+    // Handling the top placeholder
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top,
+        { onDispose: this._onDispose }
+      );
+  
+      // The extension should not assume that the expected placeholder is available.
+      if (!this._topPlaceholder) {
+        console.error("The expected placeholder (Top) was not found.");
+        return;
+      }
+  
+      if (this.properties) {
+        let topString: string = this.properties.Top;
+        if (!topString) {
+          topString = "(Top property was not defined.)";
+        }
+  
+        if (this._topPlaceholder.domElement) {
+          const element = React.createElement(Header, {
+            topString: topString // Replace with your actual string
+          });
+        
+          ReactDOM.render(element, this._topPlaceholder.domElement);
+        }
+      }
+    }
+  
+    // Handling the bottom placeholder
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Bottom,
+        { onDispose: this._onDispose }
+      );
+  
+      // The extension should not assume that the expected placeholder is available.
+      if (!this._bottomPlaceholder) {
+        console.error("The expected placeholder (Bottom) was not found.");
+        return;
+      }
+  
+      if (this.properties) {
+        let bottomString: string = this.properties.Bottom;
+        if (!bottomString) {
+          bottomString = "(Bottom property was not defined.)";
+        }
+  
+        if (this._bottomPlaceholder.domElement) {
+          this._bottomPlaceholder.domElement.innerHTML = `
+          <div class="">
+            <div class="">
+              <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
+                bottomString
+              )}
+            </div>
+          </div>`;
+        }
+      }
+    }
+  }
 
   @override
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
-    }
-
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
 
     return Promise.resolve();
   }
