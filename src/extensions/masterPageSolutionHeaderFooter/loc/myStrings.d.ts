db90db54df22da3c4994379fefbfd241c6cfc764
Anderson Moroni <andersonmoroni92@gmail.com>
2024-08-14 19:19:37 -0300

    chore: primeiro commit

---
+++ src/extensions/masterPageSolutionHeaderFooter/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IMasterPageSolutionHeaderFooterApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'MasterPageSolutionHeaderFooterApplicationCustomizerStrings' {
+  const strings: IMasterPageSolutionHeaderFooterApplicationCustomizerStrings;
+  export = strings;
+}
